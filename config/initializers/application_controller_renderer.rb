9d89c8d383423b18c50e6086c66999fbec219126
monqcq <takuyaaaan23@gmail.com>
2021-03-13 09:55:05 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '544c803e5e0db80d24d3ad979eb0bebbaf4f2a24d7bd41c0e3638f16aba3ff524dfa4a98aac2060c19a78d9480832c965734994880a7bf7a4b5fa16a39b3eaa4'
